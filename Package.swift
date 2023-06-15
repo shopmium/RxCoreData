52032b1f328cb1686f0d317972d3249cc60e179c
Allan Vialatte <PoissonBallon@users.noreply.github.com>
2023-06-15 14:03:31 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .library(name: "RxCoreData", targets: ["RxCoreData"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0"))
   ],
   targets: [
     .target(
